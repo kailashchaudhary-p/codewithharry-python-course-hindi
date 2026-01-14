f17d667e1caf6252f9863f5d7bbc1d0a48397889
kailashchaudhary-p <chaudharykailash185@gmail.com>
2026-01-13 20:36:17 -0800

    updated

---
+++ chapter 2 prectice/problem 5 .py
@@ -0,0 +1,3 @@
+a = int(input("Enter the First Number :"))
+b = int(input("Enter the Second Number :"))
+print("the avrage of this two number is :", (a + b) / 2)
---
+++ chapter 2 prectice/problem 6 .py
@@ -0,0 +1,2 @@
+a = int(input("Enter Your Number:"))
+print("The Squre If This Number is :",a*a)
