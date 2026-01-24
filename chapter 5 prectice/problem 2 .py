d0e775e33c89d88309b08b201dcaeaba93170bbf
kailashchaudhary-p <chaudharykailash185@gmail.com>
2026-01-23 21:39:49 -0800

    update

---
+++ chapter 5 prectice/problem 2 .py
@@ -0,0 +1,18 @@
+#Write a program to input eight numbers from the user and display all the unique numbers (once).
+s= set()#create a empty set
+n =input("Enter the number:")#input the numbers
+s.add(int(n))#add number in the set
+n =input("Enter the number:")#input the numbers
+s.add(int(n))#add number in the set
+n =input("Enter the number:")#input the numbers
+s.add(int(n))#add number in the set
+n =input("Enter the number:")#input the numbers
+s.add(int(n))#add number in the set
+n =input("Enter the number:")#input the numbers
+s.add(int(n))#add number in the set
+n =input("Enter the number:")#input the numbers
+s.add(int(n))#add number in the set
+n =input("Enter the number:")#input the numbers
+s.add(int(n))#add number in the set
+
+print(s)#print the set 
---
+++ chapter 5 prectice/problem 3 .py
@@ -0,0 +1,5 @@
+#Can we have a set with 18 (int) and '18' (str) as a value in it?
+S = set()#create an empty set 
+S.add(18)#add int 
+S.add("18")#add str 
+print(S)#print the set
