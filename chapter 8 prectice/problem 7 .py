656937948148f9e4882ea950fec4bd01c5e45d2e
kailashchaudhary-p <chaudharykailash185@gmail.com>
2026-02-03 21:15:35 -0800

    update

--- chapter 8 prectice/problem 7 .py
+++ chapter 8 prectice/problem 7 .py
@@ -1,2 +1,7 @@
 #Write a python function to remove a given word from a list ad strip it at the same time.
-def remove_word_and_strip()
+def rem(l,word):
+    for i in l:
+        l .remove(word)
+    return l
+l = ['apple','hello','happy']
+print(rem(l,'happy'))
