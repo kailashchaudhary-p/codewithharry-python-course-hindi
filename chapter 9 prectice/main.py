91293335d50787477b79b4109a76264e85e15fdb
kailashchaudhary-p <chaudharykailash185@gmail.com>
2026-02-04 21:58:29 -0800

    update

---
+++ chapter 9 prectice/main.py
@@ -0,0 +1,16 @@
+'''We all have played snake, water gun game in our childhood. If you haven’t, google the rules of this game and write a python program capable of playing this game with the user.'''
+import random
+def game(com,you):
+    if com==you:
+        print('Tie')
+    elif(com=='s'and you=='w')or(com=='w'and you=='g')or(com=='g'and you=='s'):
+        print('Computer wins')
+    else:
+        print('You win')
+        return game(com_dec,you)
+print("Welcome to Snake, Water, Gun game!")
+print("Enter 's' for Snake, 'w' for Water and 'g' for Gun")
+com_dec=random.choice(['s','w','g'])
+you=input("your turn:")
+
+game_dec={'s':1,'w':2,'g':3}
