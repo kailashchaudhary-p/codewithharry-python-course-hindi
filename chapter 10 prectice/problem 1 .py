0ab962c46a720455cf3d812a80f4b9fce075bc53
kailashchaudhary-p <chaudharykailash185@gmail.com>
2026-02-08 22:10:19 -0800

    update

--- chapter 10 prectice/problem 1 .py
+++ chapter 10 prectice/problem 1 .py
@@ -1,4 +1,9 @@
 # Write a program to read the text from a given file ‘poems.txt’ and find out whether it contains the word ‘twinkle’B
 #read the file 
-with open('h.txt','r')as f:
-    content = f.read().lower()
+f = open('chapter 10 prectice/poem.txt')#create a file object to open the file in read mode
+content=f.read()#read the content of the file
+if 'twinkle' in content:#use if else statement
+    print('twinkle is present in the file')
+else:
+    print('twinkle is not present in the file')
+f.close()#close the file object
