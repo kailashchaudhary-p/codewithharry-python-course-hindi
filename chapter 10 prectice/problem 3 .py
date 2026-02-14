9a18741d97749b97b8bdda870baa93b4e2131467
kailashchaudhary-p <chaudharykailash185@gmail.com>
2026-02-13 22:34:18 -0800

    update

--- chapter 10 prectice/problem 3 .py
+++ chapter 10 prectice/problem 3 .py
@@ -1,4 +1,18 @@
 #Write a program to generate multiplication tables from 2 to 20 and write it to the different files. Place these files in a folder for a 13 – year old.
-def multiplication_table(n):#create the function multiplication 
+def multi(n):
+    table = ""
+    for i in range(1,11):
+        table+=(f"{n} x{i}={n*i}\n")
+        with open(f"table of {n}.txt","w") as f:
+            f.write(table)
+    return table
+for i in range(2,21):
+    print(multi(i))
+    
+
+
+
+
+
     
      
