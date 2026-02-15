6dcb82dda4bdfec7e0d7245365ee0b13694189aa
kailashchaudhary-p <chaudharykailash185@gmail.com>
2026-02-14 22:38:48 -0800

    update

--- chapter 10 prectice/problem 6 .py
+++ chapter 10 prectice/problem 6 .py
@@ -1,1 +1,12 @@
 #Write a program to mine a log file and find out whether it contains ‘python’.
+with open ("chapter 10 prectice/log.txt")as f:
+    content=f.read()
+
+    if("python" in content):
+        print("yes python is present")
+    else:
+        print("python is not present")
+   
+
+
+
