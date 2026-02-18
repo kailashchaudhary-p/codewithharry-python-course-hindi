0df89f85d3a75bd84292f4ae9652a6e66325f5c6
kailashchaudhary-p <chaudharykailash185@gmail.com>
2026-02-17 22:15:35 -0800

    update

---
+++ chapter 10 .py/problem 2 .py
@@ -0,0 +1,10 @@
+#Write a class “Calculator” capable of finding square, cube and square root of a number.
+
+class calculator():
+    def square(self,num):
+        return num**2
+    def cube(self,num):
+        return num**3
+    def squareroot(self,num):
+        return num**0.5
+obj=calculator()
