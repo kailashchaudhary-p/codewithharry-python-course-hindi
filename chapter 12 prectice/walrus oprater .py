bf48ae22180f21da5284105d2c6885a0ec8c6c4c
kailashchaudhary-p <chaudharykailash185@gmail.com>
2026-02-25 21:09:25 -0800

    update

---
+++ chapter 12 prectice/walrus oprater .py
@@ -0,0 +1,4 @@
+#The walrus opreartor is a new add feature in python -
+if (n := [1,2,3,4,5]):#this is the walrus operator it assigns the value of the list to n and also checks if n is true (not empty)
+    print(n)
+    print(type(n))
