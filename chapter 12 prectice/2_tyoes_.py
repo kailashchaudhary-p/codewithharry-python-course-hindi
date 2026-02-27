76e356a282cf56eec458b907d4b3fa41a7473443
kailashchaudhary-p <chaudharykailash185@gmail.com>
2026-02-26 22:47:09 -0800

    update

---
+++ chapter 12 prectice/2_tyoes_.py
@@ -0,0 +1,6 @@
+#n is a variable of type int using this type we use all methods of int or string etc 
+n : int =1
+n.bit_length()#this is a method of int type it returns the number of bits necessary to represent an integer in binary, excluding the sign and leading zeros.
+a : str= "hello "
+a.upper()
+print(a,n)
